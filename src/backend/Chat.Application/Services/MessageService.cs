b621adb16b1809e2f53785dd27285f64f551683a
InLook7 <vadym.exi@gmail.com>
2025-05-07 01:31:10 +0300

    add logging

--- src/backend/Chat.Application/Services/MessageService.cs
+++ src/backend/Chat.Application/Services/MessageService.cs
@@ -4,33 +4,44 @@ using Chat.Application.Mappers;
 using Chat.Domain.Entities;
 using Chat.Domain.Interfaces;
 using FluentResults;
+using Microsoft.Extensions.Logging;
 
 namespace Chat.Application.Services;
 
 public class MessageService : IMessageService
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly ILogger<MessageService> _logger;
 
-    public MessageService(IUnitOfWork unitOfWork)
+    public MessageService(
+        IUnitOfWork unitOfWork,
+        ILogger<MessageService> logger)
     {
         _unitOfWork = unitOfWork;
+        _logger = logger;
     }
 
     public async Task<Result<IEnumerable<MessageDto>>> GetAllByRoomIdAsync(int roomId)
     {
+        _logger.LogInformation("Getting messages for Room {roomId}...", roomId);
+
         var room = await _unitOfWork.RoomRepository.GetByIdAsync(roomId);
         if (room == null)
         {
-            return Result.Fail($"Room {roomId} does not exist");
+            _logger.LogWarning("Room {roomId} does not exist.", roomId);
+            return Result.Fail($"Room {roomId} was not found");
         }
 
         var messages = await _unitOfWork.MessageRepository.GetAllByRoomIdAsync(roomId);
 
+        _logger.LogInformation("Successfully retrieved {messageCount} messages", messages.Count());
         return Result.Ok(MessageMapper.ToMessageDto(messages));
     }
 
     public async Task<MessageDto> CreateAsync(MessageDto messageDto)
     {
+        _logger.LogInformation("Creating a new message by User {userId}...", messageDto.UserId);
+
         var message = new Message
         {
             Content = messageDto.Content,
@@ -42,6 +53,7 @@ public class MessageService : IMessageService
         await _unitOfWork.MessageRepository.CreateAsync(message);
         await _unitOfWork.SaveAsync();
 
+        _logger.LogInformation("Successfully created Message {messageId}", message.Id);
         return MessageMapper.ToMessageDto(message);
     }
 }
--- src/backend/Chat.Application/Services/RoomService.cs
+++ src/backend/Chat.Application/Services/RoomService.cs
@@ -2,22 +2,30 @@ using Chat.Application.Dtos;
 using Chat.Application.Interfaces;
 using Chat.Application.Mappers;
 using Chat.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace Chat.Application.Services;
 
 public class RoomService : IRoomService
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly ILogger<RoomService> _logger;
 
-    public RoomService(IUnitOfWork unitOfWork)
+    public RoomService(
+        IUnitOfWork unitOfWork,
+        ILogger<RoomService> logger)
     {
         _unitOfWork = unitOfWork;
+        _logger = logger;
     }
 
     public async Task<IEnumerable<RoomDto>> GetAllAsync()
     {
+        _logger.LogInformation("Getting rooms...");
+
         var rooms = await _unitOfWork.RoomRepository.GetAllAsync();
 
+        _logger.LogInformation("Successfully retrieved {roomCount} rooms", rooms.Count());
         return RoomMapper.ToRoomDto(rooms); 
     }
 }
